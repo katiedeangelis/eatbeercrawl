3f2a634697861870a8e9a01ff395103814468ac8
Justin Rice <jsrice7391@gmail.com>
2017-11-02 11:21:43 -0400

    Added the google Stuff

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,9 @@
 // Here is the key for the Zomato API = cd1577ead710b996f1a2d0ecae1431dd
+function initMap() {
+    var uluru = { lat: -25.363, lng: 131.044 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 3,
+        center: uluru
+    })
+}
