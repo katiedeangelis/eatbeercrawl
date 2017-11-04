b8c14dd7edcbf8810726c6f9bc0fd7954b006283
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2017-11-04 11:34:04 -0400

    added markers and info windows to markers upon click

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,37 @@
 // Here is the key for the Zomato API = cd1577ead710b996f1a2d0ecae1431dd
 function initMap() {
+    // Map options
     var options = {
-        zoom: 8,
+        zoom: 14,
         center: {
             lat: 43.0718,
             lng: -70.7626
         }
     }
 
+    // New map
     var map = new google.maps.Map(document.getElementById('map'), options);
+
+    // Content box for marker
+    var contentString = '<div id="content">' +
+    '<h6>Rira Irish Pub</h6>' +
+    '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    // Add marker
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 43.076951,
+            lng: -70.757214
+        },
+        map: map,
+        title: 'Rira Irish Pub Test'
+    });
+
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
 }
