5ff377e8090bfce94ec17dae4c6609280d5dca0d
Justin Rice <jsrice7391@gmail.com>
2017-11-06 14:38:37 -0500

    Firestore works

--- public/assets/authentication.js
+++ public/assets/authentication.js
@@ -9,8 +9,6 @@
 
  firebase.initializeApp(config);
  var provider = new firebase.auth.GoogleAuthProvider();
-
-
  const txtEmail = $("#txtEmail");
  const txtPassword = $("#txtPassword");
  const btnLogin = $("#btnLogin");
@@ -18,21 +16,10 @@
  const btnLogout = $("#btnLogout");
  const currentUser = firebase.auth().currentUser;
 
-
-
  // This is the login button.    
  $("#btnLogin").on("click", function() {
      console.log("clicked");
      firebase.auth().signInWithRedirect(provider);
-     //  const password = $("#txtPassword").val().trim();
-     //  const email = $("#txtEmail").val().trim();
-     //  // This  saves the firebase.auth() ability to just be able to call it below.
-     //  const auth = firebase.auth();
-     //  // Sign in with email and password is a method that takes two arguemnts and returns a promise
-     //  const promise = auth.signInWithEmailAndPassword(email, password);
-
-     //  //  The promise that we get back has a message property, if you get back an error, show me the error. 
-     //  promise.catch(e => console.log(e.message));
  })
 
  $("#btnSignUp").on("click", function() {
@@ -54,10 +41,7 @@
          $("#btnLogout").hide();
          $("#btnSignUp, #btnLogin").show();
      }
- })
-
-
-
+ });
  // add a realtime listener.
  //  This allows use to use firebase for the authentication. the onAuthStateChanged Method is taking the firebase user and is loging everytime there is some form of state changed within the user
  // E.x. if the user is logged in or logged out.
---
+++ public/assets/firestore.js
@@ -0,0 +1,13 @@
+var db = firebase.firestore();
+
+db.collection("users").add({
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+    })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
